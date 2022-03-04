8137cbae88b459bb685900b557ef3e25cebef6ba
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-04 10:11:03 -0600

    updated notes

--- CSharpIntermediateConstructorsAndInheritance/Car.cs
+++ CSharpIntermediateConstructorsAndInheritance/Car.cs
@@ -8,9 +8,31 @@ namespace CSharpIntermediateConstructorsAndInheritance
 {
     public class Car : Vehicle //Car class is derived from the Vehical class, in other words it will behave like an Vehicale, contaings of its properties and methods and will be able to call the methods of the Animal class.
     {
-        public Car()
+        public Car(string registrationNumber)
+            :base(registrationNumber) //A better way is to receive a parameter called registration number in the car class's constructor. So here string registration number and whatever we receive here we simply pass it to the base class
         {
-            Console.WriteLine("Car is being initialized.");
+            Console.WriteLine("Car is being initialized. {0}", registrationNumber);
         }
     }
 }
+
+
+/*
+line 12 
+Basically what's happening here is when we create an object of type car the runtime first tries to create
+
+an object of type vehicle but because the vehicle type does not have a default or prominences constructor
+
+the runtime doesn't know how to create an object of type recall.
+
+So what we need to do here is back to the core class we use the base keyword to call one of the constructors
+
+of the vehicle class and how we do it is very simple.
+
+Enter Colon.
+
+We use the base keyword open brackets.
+
+And here we go.
+
+ */
