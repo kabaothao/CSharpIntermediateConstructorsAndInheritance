aad3ceb5d2df785120f0025b6c7dc95892ba80b8
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-04 10:11:17 -0600

    added notes

--- CSharpIntermediateConstructorsAndInheritance/Program.cs
+++ CSharpIntermediateConstructorsAndInheritance/Program.cs
@@ -9,7 +9,20 @@ namespace CSharpIntermediateConstructorsAndInheritance // Note: actual namespace
     {
         static void Main(string[] args)
         {
-            var car = new Car();
+            var car = new Car("XYZ1234");
         }
     }
 }
+
+
+/*
+ There are two things you need to know about constructors when it comes to inheritance.
+
+First is that during the instantiation of an object the base class constructors are always executed
+
+first and the second is base class constructors are not inherited.
+
+So in a derived class you need to redefine your constructors.
+
+ 
+ */
